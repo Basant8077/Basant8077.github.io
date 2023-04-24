5982b826492c5a05a915636214822227c51d4ef5
Basant joshi <bsntballabhjoshi@gmail.com>
2023-04-24 18:55:11 +0530

    Js projrcts

---
+++ JS Projects/PDF Merger/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const multer = require('multer')
+const { mergepdfs } = require(`./merge.js`)
+
+const upload = multer({ dest: 'uploads/' })
+app.use('/static', express.static('public'))
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "templates/index.html"))
+})
+
+
+
+
+app.post('/merge', upload.array('pdfs', 2), async (req, res, next)=> {
+
+    console.log(req.files)
+    let d = await mergepdfs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path))
+    res.redirect(`http://localhost:3000/static/${d}.pdf`)
+    // res.send({data: req.files})
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`)
+})
